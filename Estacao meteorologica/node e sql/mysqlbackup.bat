a9fdadaf052829517e52fb9891af17a2af1300e7
leomanoell <136029466+leomanoell@users.noreply.github.com>
2023-10-31 19:19:02 -0300

    Update mysqlbackup.bat

--- Estacao meteorologica/node e sql/mysqlbackup.bat
+++ Estacao meteorologica/node e sql/mysqlbackup.bat
@@ -1,15 +1,15 @@
-rem path to mysql server bin folder
+rem Caminho para a pasta bin do servidor MySQL
 cd "C:\Program Files\MySQL\MySQL Server 8.0\bin"
 
-rem credentials to connect to mysql server
+rem Credenciais para se conectar ao servidor MySQL
 set mysql_user=root
 set mysql_password=JoaoDoPaoDoce
 
-rem backup file name generation
+rem Nome do arquivo de backup gerado
 set backup_path=F:\New Folder\MySQL Backup
 set backup_name=db_milan
 
-rem backup creation
+rem Criação do backup
 mysqldump --user=%mysql_user% --password=%mysql_password% --db_milan --routines --events --result-file="%backup_path%\%backup_name%.sql"
 if %ERRORLEVEL% neq 0 (
     (echo Backup failed: error during dump creation) >> "%backup_path%\mysql_backup_log.txt"
